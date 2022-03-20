a07b736c8fed79db33d8e9db49c95224cd8b5878
gavin <yvjinbo@qq.com>
2022-03-21 02:53:33 +0800

    修改为输入log所在目录 eg： ./main -srcpath=/home/gavin/桌面/Kmeans/slave2 输入为： /home/gavin/桌面/Kmeans/slave2/hadoop-root-datanode-slave2.log.0 /home/gavin/桌面/Kmeans/slave2/hadoop-root-datanode-slave2.log.1 /home/gavin/桌面/Kmeans/slave2/hadoop-root-datanode-slave2.log.2 输出为： /home/gavin/桌面/Kmeans/slave2/hadoop-root-datanode-slave2.log.0.json /home/gavin/桌面/Kmeans/slave2/hadoop-root-datanode-slave2.log.1.json /home/gavin/桌面/Kmeans/slave2/hadoop-root-datanode-slave2.log.2,json

--- src/main/main.go
+++ src/main/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -19,125 +20,142 @@ type wrInfo struct {
 }
 
 func main() {
+	srcpath := flag.String("srcpath", "/home/gavin/", "log日志路径")
+	//outPath := flag.String("outPath", "/home/gavin/out.txt", "输出文件路径") //eg:/home/gavin/桌面/Kmeans/slave2/res.txt
 
-	path := flag.String("path", "/home/gavin/", "log日志路径")
-	outPath := flag.String("outPath", "/home/gavin/out.txt", "输出文件路径") //eg:/home/gavin/桌面/Kmeans/slave2/res.txt
 	//解析命令行参数
 	flag.Parse()
+	//获取文件目录文件名列表
+	var files []string
+	err := filepath.Walk(*srcpath, func(path string, info os.FileInfo, err error) error {
+		//不处理文件夹
+		if info.IsDir() {
+			return nil
+		}
+		files = append(files, path)
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
 	//resolute := make([]wrInfo, 0)
 	//nodeList := make(map[string]bool)
-	// 1.打开一个文件
-	// 注意: 文件不存在不会创建, 会报错
-	// 注意: 通过Open打开只能读取, 不能写入
-	fpSrc, err := os.Open(*path)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	// 2.关闭一个文件
-	defer func() {
-		err = fpSrc.Close()
+	for _, file := range files {
+		//path := *srcpath + "/" + file
+		fmt.Println(file)
+		// 1.打开一个文件
+		// 注意: 文件不存在不会创建, 会报错
+		// 注意: 通过Open打开只能读取, 不能写入
+		fpSrc, err := os.Open(file)
 		if err != nil {
 			fmt.Println(err)
 		}
-	}()
 
-	// 1.打开文件
-	fpDest, err := os.OpenFile(*outPath, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
-	if err != nil {
-		fmt.Println(err)
-	}
-	// 2.关闭打开的文件
-	defer func() {
-		err := fpDest.Close()
+		// 2.关闭一个文件
+		defer func() {
+			err = fpSrc.Close()
+			if err != nil {
+				fmt.Println(err)
+			}
+		}()
+
+		// 1.打开文件
+		fpDest, err := os.OpenFile(file+".json", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
 		if err != nil {
 			fmt.Println(err)
 		}
-	}()
-
-	// 3.创建缓冲区
-	w := bufio.NewWriter(fpDest)
-
-	// 4.读取文件中所有内容, 直到文件末尾为止
-	r := bufio.NewReader(fpSrc)
-	for {
-		//buf, err := r.ReadBytes('\n')
-		buf, err := r.ReadString('\n')
-		if strings.Contains(buf, "HDFS_READ") {
-			res := strings.Split(buf, " ")
-			timestamp := " " + res[0] + " " + res[1]
-			blockId := res[len(res)-3]
-			srcIp := strings.Split(strings.Replace(res[5], "/", "", -1), ":")[0]
-			destIp := strings.Split(strings.Replace(res[7], "/", "", -1), ":")[0]
-			temp := wrInfo{
-				Timestamp: timestamp,
-				BlockId:   blockId,
-				SrcIP:     srcIp,
-				DestIp:    destIp,
-				Wr:        "<----读",
-			}
-			//resolute = append(resolute, temp)
-			info, err2 := json.Marshal(temp)
-			if err2 != nil {
-				fmt.Println(err2)
-			}
-			_, err := w.Write(info)
-			_, err = w.WriteString("\n")
-			if err != nil {
-				return
-			}
-			if err != nil {
-				return
-			}
+		// 2.关闭打开的文件
+		defer func() {
+			err := fpDest.Close()
 			if err != nil {
 				fmt.Println(err)
 			}
-			err = w.Flush()
-			if err != nil {
-				fmt.Println("刷新失败")
+		}()
+
+		// 3.创建写缓冲区
+		w := bufio.NewWriter(fpDest)
+
+		// 4.读取文件中所有内容, 直到文件末尾为止
+		r := bufio.NewReader(fpSrc)
+		for {
+			//buf, err := r.ReadBytes('\n')
+			buf, err := r.ReadString('\n')
+			if strings.Contains(buf, "HDFS_READ") {
+				res := strings.Split(buf, " ")
+				timestamp := " " + res[0] + " " + res[1]
+				blockId := res[len(res)-3]
+				srcIp := strings.Split(strings.Replace(res[5], "/", "", -1), ":")[0]
+				destIp := strings.Split(strings.Replace(res[7], "/", "", -1), ":")[0]
+				temp := wrInfo{
+					Timestamp: timestamp,
+					BlockId:   blockId,
+					SrcIP:     srcIp,
+					DestIp:    destIp,
+					Wr:        "<----读",
+				}
+				//resolute = append(resolute, temp)
+				info, err2 := json.Marshal(temp)
+				if err2 != nil {
+					fmt.Println(err2)
+				}
+				_, err := w.Write(info)
+				_, err = w.WriteString("\n")
+				if err != nil {
+					return
+				}
+				if err != nil {
+					return
+				}
+				if err != nil {
+					fmt.Println(err)
+				}
+				err = w.Flush()
+				if err != nil {
+					fmt.Println("刷新失败")
+				}
+				//nodeList[srcIp] = true
+				//nodeList[destIp] = true
+				//fmt.Println(timestamp, blockId, srcIp,"<----",destIp)
+				//fmt.Println(res)
 			}
-			//nodeList[srcIp] = true
-			//nodeList[destIp] = true
-			//fmt.Println(timestamp, blockId, srcIp,"<----",destIp)
-			//fmt.Println(res)
-		}
-		if strings.Contains(buf, "HDFS_WRITE") {
-			res := strings.Split(buf, " ")
-			timestamp := " " + res[0] + " " + res[1]
-			blockId := res[len(res)-3]
-			srcIp := strings.Split(strings.Replace(res[5], "/", "", -1), ":")[0]
-			destIp := strings.Split(strings.Replace(res[7], "/", "", -1), ":")[0]
-			temp := wrInfo{
-				Timestamp: timestamp,
-				BlockId:   blockId,
-				SrcIP:     srcIp,
-				DestIp:    destIp,
-				Wr:        "---->写",
+			if strings.Contains(buf, "HDFS_WRITE") {
+				res := strings.Split(buf, " ")
+				timestamp := " " + res[0] + " " + res[1]
+				blockId := res[len(res)-3]
+				srcIp := strings.Split(strings.Replace(res[5], "/", "", -1), ":")[0]
+				destIp := strings.Split(strings.Replace(res[7], "/", "", -1), ":")[0]
+				temp := wrInfo{
+					Timestamp: timestamp,
+					BlockId:   blockId,
+					SrcIP:     srcIp,
+					DestIp:    destIp,
+					Wr:        "---->写",
+				}
+				//resolute = append(resolute, temp)
+				info, err2 := json.Marshal(temp)
+				if err2 != nil {
+					fmt.Println(err2)
+				}
+				_, err := w.Write(info)
+				_, err = w.WriteString("\n")
+				if err != nil {
+					return
+				}
+				if err != nil {
+					fmt.Println(err)
+				}
+				err = w.Flush()
+				if err != nil {
+					fmt.Println("刷新失败")
+				}
+				//nodeList[srcIp] = true
+				//nodeList[destIp] = true
+				//fmt.Println(timestamp, blockId, srcIp,"---->",destIp)
+				//fmt.Println(res)
 			}
-			//resolute = append(resolute, temp)
-			info, err2 := json.Marshal(temp)
-			if err2 != nil {
-				fmt.Println(err2)
+			if err == io.EOF {
+				break
 			}
-			_, err := w.Write(info)
-			_, err = w.WriteString("\n")
-			if err != nil {
-				return
-			}
-			if err != nil {
-				fmt.Println(err)
-			}
-			err = w.Flush()
-			if err != nil {
-				fmt.Println("刷新失败")
-			}
-			//nodeList[srcIp] = true
-			//nodeList[destIp] = true
-			//fmt.Println(timestamp, blockId, srcIp,"---->",destIp)
-			//fmt.Println(res)
-		}
-		if err == io.EOF {
-			break
 		}
 	}
 
