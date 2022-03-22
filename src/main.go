ea2e8c4f23243019ec8816276d14eb16d1ac98a5
gavin <yvjinbo@qq.com>
2022-03-22 17:41:41 +0800

    基本主体已经完成.可以添加缓存列表。(部分功能修改为函数实现) go run main.go --maxCaches=1,2,4,8 -srcfile=/home/gavin/桌面/test/srcfileMap.json 结果： 命中率：6.411398040961709% 命中率：11.84327693677649% 命中率：28.584149599287624% 命中率：68.83348174532502%

--- src/main.go
+++ src/main.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"os"
 	"src/policy/lru"
+	"strconv"
+	"strings"
 )
 
 type kv struct {
@@ -16,8 +18,8 @@ type kv struct {
 
 func main() {
 	srcfile := flag.String("srcfile", "/home/gavin/桌面/test/srcfileKV.json", "K-V文件")
-	//maxCaches := flag.Int("maxCaches", "1,2,4,8,16", "最大缓存容量")
-	maxCache := flag.Int("maxCache", 8, "最大缓存容量")
+	maxCaches := flag.String("maxCaches", "1,2,4,8,16", "最大缓存容量")
+	//maxCache := flag.Int("maxCache", 8, "最大缓存容量")
 	//policies := flag.String("policies", "lru,lfu,fifo,mru,clock", "页面置换算法")
 	flag.Parse()
 
@@ -32,28 +34,27 @@ func main() {
 	var m []kv
 	readString, _ := r.ReadString('\n')
 	json.Unmarshal([]byte(readString), &m)
-	//maxCacheList := strings.Split(*maxCaches, ",")
-	//for _, maxCache := range maxCacheList {
-	//
-	//}
-	cache_hit_ratio(m, *maxCache)
+	maxCacheList := strings.Split(*maxCaches, ",")
+	for _, v := range maxCacheList {
+		maxCache, _ := strconv.ParseInt(v, 10, 64)
+		cache_hit_ratio(m, maxCache)
+	}
 
 }
 
-func cache_hit_ratio(m []kv, maxCache int) {
+func cache_hit_ratio(m []kv, maxCache int64) {
 	hit, unhit := 0, 0
-	cache := lru.NewCache[string, string](lru.WithCapacity(maxCache))
+	cache := lru.NewCache[string, string](lru.WithCapacity(int(maxCache)))
 	for _, kv := range m {
-		got, ok := cache.Get(kv.BlockId)
+		_, ok := cache.Get(kv.BlockId)
 		if ok {
-			fmt.Println("命中-----" + got)
+			//fmt.Println("命中-----" + got)
 			hit++
 		} else {
-			fmt.Println("未命中----" + got)
+			//fmt.Println("未命中----" + got)
 			unhit++
 			cache.Set(kv.BlockId, kv.DestIp)
 		}
-		cache.Show()
 	}
 	var res float64
 	res = float64(hit) / float64(hit+unhit)
