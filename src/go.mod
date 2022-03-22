e1cfe972a8d76423019d157daefb39504d7dc0e7
gavin <yvjinbo@qq.com>
2022-03-22 23:09:58 +0800

    主体部分已经完成。 readme文档已经添加。

--- src/go.mod
+++ src/go.mod
@@ -1,9 +1,22 @@
 module src
 
 require (
-	src/lru v0.0.1
+	github.com/Code-Hex/go-generics-cache v1.0.0
+	src/policy/clock v0.0.1
+	src/policy/fifo v0.0.1
+	src/policy/lfu v0.0.1
+	src/policy/lru v0.0.1
+	src/policy/mru v0.0.1
 )
+
+require golang.org/x/exp v0.0.0-20220209042442-160e291fcf24 // indirect
+
 replace (
-	src/lru => ./lru
+	src/policy/clock => ./policy/clock
+	src/policy/fifo => ./policy/fifo
+	src/policy/lfu => ./policy/lfu
+	src/policy/lru => ./policy/lru
+	src/policy/mru => ./policy/mru
 )
-go 1.17
+
+go 1.18
--- src/main.go
+++ src/main.go
@@ -6,7 +6,11 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"src/policy/clock"
+	"src/policy/fifo"
+	"src/policy/lfu"
 	"src/policy/lru"
+	"src/policy/mru"
 	"strconv"
 	"strings"
 )
@@ -20,9 +24,9 @@ func main() {
 	srcfile := flag.String("srcfile", "/home/gavin/桌面/test/srcfileKV.json", "K-V文件")
 	maxCaches := flag.String("maxCaches", "1,2,4,8,16", "最大缓存容量")
 	//maxCache := flag.Int("maxCache", 8, "最大缓存容量")
-	//policies := flag.String("policies", "lru,lfu,fifo,mru,clock", "页面置换算法")
+	policies := flag.String("policies", "lru,lfu,fifo,mru,clock", "页面置换算法")
 	flag.Parse()
-
+	//policies := []string{"lru", "lfu", "mru", "fifo", "clock"}
 	fd, open := os.Open(*srcfile)
 	if open != nil {
 		fmt.Println(open)
@@ -35,29 +39,146 @@ func main() {
 	readString, _ := r.ReadString('\n')
 	json.Unmarshal([]byte(readString), &m)
 	maxCacheList := strings.Split(*maxCaches, ",")
+	policylist := strings.Split(*policies, ",")
 	for _, v := range maxCacheList {
-		maxCache, _ := strconv.ParseInt(v, 10, 64)
-		cache_hit_ratio(m, maxCache)
-	}
-
-}
-
-func cache_hit_ratio(m []kv, maxCache int64) {
-	hit, unhit := 0, 0
-	cache := lru.NewCache[string, string](lru.WithCapacity(int(maxCache)))
-	for _, kv := range m {
-		_, ok := cache.Get(kv.BlockId)
-		if ok {
-			//fmt.Println("命中-----" + got)
-			hit++
-		} else {
-			//fmt.Println("未命中----" + got)
-			unhit++
-			cache.Set(kv.BlockId, kv.DestIp)
+		for _, policy := range policylist {
+			maxCache, _ := strconv.ParseInt(v, 10, 64)
+			cache_hit_ratio(m, maxCache, policy)
 		}
 	}
-	var res float64
-	res = float64(hit) / float64(hit+unhit)
-	res *= 100
-	fmt.Printf("命中率：%v%%\n", res)
+
 }
+func cache_hit_ratio(m []kv, maxCache int64, policy string) {
+	switch policy {
+	case "lru":
+		cache := lru.NewCache[string, string](lru.WithCapacity(int(maxCache)))
+		hit, unhit := 0, 0
+		for _, kv := range m {
+			_, ok := cache.Get(kv.BlockId)
+			if ok {
+				//fmt.Println("命中-----" + got)
+				hit++
+			} else {
+				//fmt.Println("未命中----" + got)
+				unhit++
+				cache.Set(kv.BlockId, kv.DestIp)
+			}
+		}
+		var res float64
+		res = float64(hit) / float64(hit+unhit)
+		res *= 100
+		fmt.Printf("当前容量：%v;当前置换算法的:%v;命中率：%v%%\n", maxCache, policy, res)
+	case "lfu":
+		cache := lfu.NewCache[string, string](lfu.WithCapacity(int(maxCache)))
+		hit, unhit := 0, 0
+		for _, kv := range m {
+			_, ok := cache.Get(kv.BlockId)
+			if ok {
+				//fmt.Println("命中-----" + got)
+				hit++
+			} else {
+				//fmt.Println("未命中----" + got)
+				unhit++
+				cache.Set(kv.BlockId, kv.DestIp)
+			}
+		}
+		var res float64
+		res = float64(hit) / float64(hit+unhit)
+		res *= 100
+		fmt.Printf("当前容量：%v;当前置换算法的:%v;命中率：%v%%\n", maxCache, policy, res)
+	case "mru":
+		cache := mru.NewCache[string, string](mru.WithCapacity(int(maxCache)))
+		hit, unhit := 0, 0
+		for _, kv := range m {
+			_, ok := cache.Get(kv.BlockId)
+			if ok {
+				//fmt.Println("命中-----" + got)
+				hit++
+			} else {
+				//fmt.Println("未命中----" + got)
+				unhit++
+				cache.Set(kv.BlockId, kv.DestIp)
+			}
+		}
+		var res float64
+		res = float64(hit) / float64(hit+unhit)
+		res *= 100
+		fmt.Printf("当前容量：%v;当前置换算法的:%v;命中率：%v%%\n", maxCache, policy, res)
+	case "fifo":
+		cache := fifo.NewCache[string, string](fifo.WithCapacity(int(maxCache)))
+		hit, unhit := 0, 0
+		for _, kv := range m {
+			_, ok := cache.Get(kv.BlockId)
+			if ok {
+				//fmt.Println("命中-----" + got)
+				hit++
+			} else {
+				//fmt.Println("未命中----" + got)
+				unhit++
+				cache.Set(kv.BlockId, kv.DestIp)
+			}
+		}
+		var res float64
+		res = float64(hit) / float64(hit+unhit)
+		res *= 100
+		fmt.Printf("当前容量：%v;当前置换算法的:%v;命中率：%v%%\n", maxCache, policy, res)
+	case "clock":
+		cache := clock.NewCache[string, string](clock.WithCapacity(int(maxCache)))
+		hit, unhit := 0, 0
+		for _, kv := range m {
+			_, ok := cache.Get(kv.BlockId)
+			if ok {
+				//fmt.Println("命中-----" + got)
+				hit++
+			} else {
+				//fmt.Println("未命中----" + got)
+				unhit++
+				cache.Set(kv.BlockId, kv.DestIp)
+			}
+		}
+		var res float64
+		res = float64(hit) / float64(hit+unhit)
+		res *= 100
+		fmt.Printf("当前容量：%v;当前置换算法的:%v;命中率：%v%%\n", maxCache, policy, res)
+	default:
+		//默认LRU
+		cache := lru.NewCache[string, string](lru.WithCapacity(int(maxCache)))
+		hit, unhit := 0, 0
+		for _, kv := range m {
+			_, ok := cache.Get(kv.BlockId)
+			if ok {
+				//fmt.Println("命中-----" + got)
+				hit++
+			} else {
+				//fmt.Println("未命中----" + got)
+				unhit++
+				cache.Set(kv.BlockId, kv.DestIp)
+			}
+		}
+		var res float64
+		res = float64(hit) / float64(hit+unhit)
+		res *= 100
+		fmt.Printf("当前容量：%v;当前置换算法的:%v;命中率：%v%%\n", maxCache, policy, res)
+	}
+
+}
+
+//func cache_hit_ratio(m []kv, maxCache int64) {
+//	hit, unhit := 0, 0
+//	cache := lru.NewCache[string, string](lru.WithCapacity(int(maxCache)))
+//	for _, kv := range m {
+//		_, ok := cache.Get(kv.BlockId)
+//		if ok {
+//			//fmt.Println("命中-----" + got)
+//			hit++
+//		} else {
+//			//fmt.Println("未命中----" + got)
+//			unhit++
+//			cache.Set(kv.BlockId, kv.DestIp)
+//		}
+//	}
+//	var res float64
+//	res = float64(hit) / float64(hit+unhit)
+//	res *= 100
+//	fmt.Printf("命中率：%v%%\n", res)
+//}
